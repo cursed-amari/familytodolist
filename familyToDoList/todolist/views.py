96004f4a562eee3f6a8fb446e35714925f73b731
Amari <filin1232@gmail.com>
2024-05-16 16:05:00 +0300

    full output

--- familyToDoList/todolist/views.py
+++ familyToDoList/todolist/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from .models import ToDoListModel
+from .models import *
 
 
 def view_tasks(request):
     user = request.user
     personal_tasks = ToDoListModel.objects.filter(created_by=user, task_type='personal')
-    # shared_tasks = ToDoListModel.objects.filter(task_type='shared', shared_with__user=user)
+
+    # Получение всех общих задач, включая подзадачи
+    shared_tasks = TaskShare.objects.filter(user=user)
+    shared_task_ids = shared_tasks.values_list('task__id', flat=True)
+    shared_subtasks = SubTask.objects.filter(task_id__in=shared_task_ids)
+
     context = {
         'personal_tasks': personal_tasks,
-        # 'shared_tasks': shared_tasks,
+        'shared_tasks': shared_tasks,
+        'shared_subtasks': shared_subtasks,  # Передача общих подзадач в контекст
     }
     return render(request, 'todolist/todolist.html', context)
